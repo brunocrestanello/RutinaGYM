1d4811e9cefe9616ccd8d37f233ffd0a0cccf5f4
Bruno Crestanello <80794540+brunocrestanello@users.noreply.github.com>
2026-05-20 16:49:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
 const CACHE = 'rutina-bruno-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
+const ASSETS = [
+  '/RutinaGYM/',
+  '/RutinaGYM/index.html',
+  '/RutinaGYM/manifest.json'
+];
+ 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
+ 
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
